15b8f063375427450dab72af3dc408a3895ec5e6
luqax <106706839+luqax@users.noreply.github.com>
2023-03-06 15:47:30 -0300

    Minecraft Heads - fronted 001.3

--- client/script.js
+++ client/script.js
@@ -10,16 +10,25 @@ async function getAPI(){
     setTimeout(() => {
         const data = JSON.parse(httpGet("http://localhost:3001/banlist"));
         
-        data.map((d) => {
+        data.map(async (d) => {
+            const response = await fetch(`http://cravatar.eu/head/${d.name}/128.png`)
+            const data1 = await response.text()
+            console.log(data1)
+
+            const img = new Image();
+            img.src = data1
             const list = document.getElementById("list");
             const entry = document.createElement("li");
             const bg = document.createElement("div");
             bg.className = "slotBg";
+            const head = document.createElement("img");
+            head.src = img
             const nameText = document.createElement("p");
             nameText.className = "nameText";
             nameText.innerHTML = d.name
-            entry.appendChild(bg)
+            entry.appendChild(bg);
             bg.appendChild(nameText);
+            bg.appendChild(img);
             list.appendChild(entry);
         }) 
     }, 1000)
